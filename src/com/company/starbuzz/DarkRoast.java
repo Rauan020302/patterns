dcf0aa2b575288a81c27204cd9d359677d2e4c69
Рауан <ibragimov020302@gmail.com>
2021-06-23 20:29:08 +0600

    decorator

---
+++ src/com/company/starbuzz/DarkRoast.java
@@ -0,0 +1,14 @@
+package com.company.starbuzz;
+
+
+public class DarkRoast extends Beverage {
+
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 0.99;
+    }
+}
---
+++ src/com/company/starbuzz/Mocha.java
@@ -0,0 +1,19 @@
+package com.company.starbuzz;
+
+public class Mocha extends CondimentDecorator{
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", mocha";
+    }
+
+    @Override
+    public double cost() {
+        return 0.20 + beverage.cost();
+    }
+}
