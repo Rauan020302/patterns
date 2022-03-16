1f48d32c5ff9608aac7af99f958b6247f312c3c6
Maksut Rauan <Ibragimov02@Gmail.com>
2022-03-16 23:32:05 +0300

    chain of responsibility with example ATM

---
+++ src/com/company/chainOfResponsibility/ChainOfResponsibilityApp.java
@@ -0,0 +1,52 @@
+package com.company.chainOfResponsibility;
+
+public class ChainOfResponsibilityApp {
+    public static void main(String[] args) {
+        Logger smsLogger = new SMSLogger(Level.ERROR);
+        Logger fileLogger = new FileLogger(Level.DEBUG);
+        Logger emailLogger = new EmailLogger(Level.INFO);
+        smsLogger.setNext(fileLogger);
+        fileLogger.setNext(emailLogger);
+
+        smsLogger.writeMessage("All OK",Level.INFO);
+        smsLogger.writeMessage("DEBUG",Level.DEBUG);
+        smsLogger.writeMessage("All BAD",Level.ERROR);
+    }
+}
+class Level{
+    public static final int ERROR = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+}
+abstract class Logger{
+    int priority;
+    Logger next;
+
+    public Logger(int priority) {
+        this.priority = priority;
+    }
+    public void setNext(Logger next) {
+        this.next = next;
+    }
+    public void writeMessage(String message, int level) {
+        if (level <= priority){
+            write(message);
+        }
+        if (next != null){
+            next.writeMessage(message,level);
+        }
+    }
+    abstract void write(String message);
+}
+class SMSLogger extends Logger {
+    public SMSLogger(int priority) {super(priority);}
+    public void write(String message) {System.out.println("SMS: " + message);}
+}
+class FileLogger extends Logger {
+    public FileLogger(int priority) {super(priority);}
+    public void write(String message) {System.out.println("File: " + message);}
+}
+class EmailLogger extends Logger {
+    public EmailLogger(int priority) {super(priority);}
+    public void write(String message) {System.out.println("Email: " + message);}
+}
